1a79e44e3924ebe45f21f079f761bd4fe602bf74
Igor Martins <igor.martins@tecnospeed.com.br>
2018-07-16 08:58:40 -0300

    fix: next/previous

--- store/api.js
+++ store/api.js
@@ -16,7 +16,7 @@ const methods = {
         })
     },
     nextTrack: function ({ index, channel }, cb) {
-        console.log(index + 1)
+        if (index === 0) index = -1
         Vue.http.get(`${url.prod}/tracks/?offset=${index + 1}&timestamp=1531438026840&channel=${channel}`).then(response => {
             let track = response.body
 
@@ -27,6 +27,7 @@ const methods = {
         })
     },
     previousTrack: function ({ index, channel }, cb) {
+        if (index === 0) index = -1
         Vue.http.get(`${url.prod}/tracks/?offset=${index - 1}&timestamp=1531438026840&channel=${channel}`).then(response => {
             let track = response.body
 
